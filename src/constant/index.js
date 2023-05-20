54925b0badac2bb1793bca67772fda3d10879c8c
bqviet86 <bqvct4211@gmail.com>
2023-05-20 17:12:48 +0700

    update proxy url

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,1 +1,1 @@
-export const imgProxyUrl = 'https://kyt-image-prx.onrender.com/proxy?url=http://www.nettruyenme.com&src='
+export const imgProxyUrl = 'https://animanlabv2.online/kytserver/api/api/v2/proxy?url=https://www.nettruyenco.vn&src='
